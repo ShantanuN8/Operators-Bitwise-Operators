66e7ed0ad0c1298827a370bef42a67cf30ad7cf5
Shantanu Nawathey <139308945+ShantanuN8@users.noreply.github.com>
2023-10-21 14:20:55 +0530

    Update and rename Basic Operators to Basic Operators.cpp

---
+++ Basic Operators.cpp
@@ -0,0 +1,35 @@
+//22070123107
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World\n";
+    int a,b,sum,sub,div,mul;
+   cout<<"Enter a number\n";
+   cin>>a;
+   cout<<"Enter another number\n";
+   cin>>b;
+   sum=a+b;
+   sub=a-b;
+   div=a/b;
+   mul=a*b;
+   cout<<"Addition \n"<<sum<<"\n" ;
+   cout<<"Substraction\n"<<sub<<"\n" ;
+   cout<<"Division\n"<<div<<"\n" ;
+   cout<<"Multiplication\n"<<mul<<"\n";
+}
+/*OUTPUT
+Hello World
+Enter a number
+2
+Enter another number
+4
+Addition 
+6
+Substraction
+-2
+Division
+0
+Multiplication
+8
+*/
